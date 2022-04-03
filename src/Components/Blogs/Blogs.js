9491511df84c0c021fc4105924da84c33d4db6e9
Ramash3828 <ramash3828@gmail.com>
2022-04-04 05:48:34 +0600

    question and answer part done

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -12,11 +12,56 @@ const Blogs = () => {
                             <p>
                                 <strong>What is Context API?</strong>
                             </p>
-                            <p></p>
+                            <p>
+                                Ans:- Context API is the way to create global
+                                variables that can be passed around the
+                                component. Context API is an alternative to
+                                passing props manually in a component. The
+                                Context API is a React structure that enables
+                                you to exchange unique data and enable to
+                                solving prop-drilling from all levels of your
+                                projects. It's can easy to passing data parent
+                                component to other siblings component or child
+                                components.
+                            </p>
                         </div>
                     </div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                        <div className="blog-text text-start">
+                            <p>
+                                <strong>What is Semantic Tag?</strong>
+                            </p>
+                            <p>
+                                Ans:- Semantic HTML elements are those that
+                                clearly describe their meaning in a human- and
+                                machine-readable way. Elements such as header ,
+                                footer and article are all semantic Tags because
+                                they clearly describe the purpose of the element
+                                and the type of content that is inside them.A
+                                semantic element clearly describes its meaning
+                                to both the browser and the developer.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="blog-text text-start">
+                            <p>
+                                <strong>
+                                    What is block Elements and Inline Elements?
+                                </strong>
+                            </p>
+                            <p>
+                                Ans:- A block-level element always starts on a
+                                new line and takes the full width. An inline
+                                element does not start on a new line and it only
+                                takes as much width. The DIV element is a
+                                block-level. If we want to convert block level
+                                to Inline level, we can use display property and
+                                value inline-block and other hand inline to
+                                block then display: block.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
