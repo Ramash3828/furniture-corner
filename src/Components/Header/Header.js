42ac1a35b81dc51b8c95eb0d154fb62b83eea6fb
Ramash3828 <ramash3828@gmail.com>
2022-04-03 22:46:21 +0600

    navbar ruteing done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="myNavbar">
+            <nav className="navbar navbar-expand-lg  container">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">
+                        Navbar
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon text-dark"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarSupportedContent"
+                    >
+                        <ul className="navbar-nav  mb-2 mb-lg-0 ms-auto">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) =>
+                                        isActive ? isActive.active : "white"
+                                    }
+                                >
+                                    HOME
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/reviews"
+                                    className={({ isActive }) =>
+                                        isActive ? isActive.active : "white"
+                                    }
+                                >
+                                    REVIEWS
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/dashboard"
+                                    className={({ isActive }) =>
+                                        isActive ? isActive.active : "white"
+                                    }
+                                >
+                                    DASHBOARD
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/blogs"
+                                    className={({ isActive }) =>
+                                        isActive ? isActive.active : "white"
+                                    }
+                                >
+                                    BLOGS
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+const render = () => {
+    root.render(
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </BrowserRouter>
+    );
+};
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
